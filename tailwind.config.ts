19f60b8edc3a2b61473b371a2e6a3d749253eb0d
alexey.tavgen <tavgen.alexey@gmail.com>
2025-10-04 09:20:22 +0200

    fix config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from 'tailwindcss';
+import type { TailwindConfig } from "tailwindcss/tailwind-config";
 
-const config: Config = {
+const config: TailwindConfig = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
